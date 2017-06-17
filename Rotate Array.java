76ac64fc8c9aa1835f5a00e2ff83b14569092a93
mukuljadhav <mxdnjadhav@gmail.com>
2017-06-17 12:12:00 -0700

    Rotate Array

---
+++ Rotate Array.java
@@ -0,0 +1,26 @@
+/*
+
+189. Rotate Array
+
+Description: Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+*/
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length, j = 0;
+        k = k % n;
+		int[] result = new int[nums.length];
+		for (int i = 0; i < n - k; i++)
+			result[i + k] = nums[i];
+
+		for (int i = n - k; i < n; i++)
+			result[j++] = nums[i];
+
+		for (int i = 0; i < result.length; i++)
+			nums[i] = result[i];
+		
+    }
+}
