6cc11ef9e2aa325f364d1de1d3421046b92bf7d5
Mukul Jadhav <mukulsingh.jadhav@asu.edu>
2017-10-16 19:37:28 -0700

    Linked List Cycle II

---
+++ Linked List Cycle II.java
@@ -0,0 +1,42 @@
+/*
+
+142. Linked List Cycle II
+
+Description: Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                ListNode slow2 = head; 
+                while (slow2 != slow){
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+
